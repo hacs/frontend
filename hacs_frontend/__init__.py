06107e390842cd166a8341e8b1ea948ec86bad8c
ludeeus <joasoe@gmail.com>
2019-11-17 09:37:32 +0000

    Fix path

--- hacs_frontend/__init__.py
+++ hacs_frontend/__init__.py
@@ -1,11 +1,10 @@
 """HACS Frontend"""
-import os
 
 def locate_dir():
-    return os.getcwd()
+    return __path__
 
 def locate_js():
-    return f"{locate_dir()}/main.js"
+    return f"{__path__}/main.js"
 
 def locate_gz():
-    return f"{locate_dir()}/main.js.gz"
+    return f"{__path__}/main.js.gz"
