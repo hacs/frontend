25776f9c8820a9ee1b23434a9a88eea5de5d180e
ludeeus <joasoe@gmail.com>
2019-11-17 09:26:59 +0000

    Fix filenames

--- hacs_frontend/__init__.py
+++ hacs_frontend/__init__.py
@@ -1,6 +1,11 @@
 """HACS Frontend"""
 import os
 
-def location():
-    """Return the location of the frontend."""
-    return f"{os.getcwd()}/main.js"
+def locate_dir():
+    return os.getcwd()
+
+def locate_js():
+    return f"{locate_dir()}/main.js"
+
+def locate_gz():
+    return f"{locate_dir()}/main.js.gz"
