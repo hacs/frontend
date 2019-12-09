e1d919d01d490244ef1c745b3731e523913df83f
ludeeus <joasoe@gmail.com>
2019-12-09 17:20:42 +0000

    use GFMD for LL hint

--- src/misc/LoveLaceHint.ts
+++ src/misc/LoveLaceHint.ts
@@ -3,6 +3,8 @@ import { Configuration, Repository } from "../types"
 import { HacsStyle } from "../style/hacs-style"
 import { HomeAssistant } from "custom-card-helpers";
 
+import { GFM } from '../markdown/styles';
+
 @customElement("hacs-lovelace-hint")
 export class LoveLaceHint extends LitElement {
     @property() public hass!: HomeAssistant;
@@ -11,11 +13,11 @@ export class LoveLaceHint extends LitElement {
 
     protected render(): TemplateResult | void {
         return html`
-            <div class="lovelace-hint">
+            <div class="lovelace-hint markdown-body">
                 <p class="example-title">${this.hass.localize(`component.hacs.repository.lovelace_instruction`)}:</p>
                 <pre id="LovelaceExample" class="yaml">
-- url: /community_plugin/${this.repository.full_name.split("/")[1]}/${this.repository.file_name}
-  type: ${(this.repository.javascript_type !== undefined
+  - url: /community_plugin/${this.repository.full_name.split("/")[1]}/${this.repository.file_name}
+    type: ${(this.repository.javascript_type !== undefined
                 ? html`${this.repository.javascript_type}`
                 : html`${this.hass.localize(`component.hacs.repository.lovelace_no_js_type`)}`)}</pre>
 
@@ -40,7 +42,7 @@ export class LoveLaceHint extends LitElement {
         document.execCommand('copy');
     }
     static get styles(): CSSResultArray {
-        return [HacsStyle, css`
+        return [HacsStyle, GFM, css`
             .lovelace-hint {
 
             }
@@ -48,11 +50,6 @@ export class LoveLaceHint extends LitElement {
                 margin-block-end: 0em;
             }
             .yaml {
-                font-family: monospace, monospace;
-                font-size: 1em;
-                border-style: solid;
-                border-width: thin;
-                margin: 0;
                 overflow: auto;
                 display: inline-flex;
                 width: calc(100% - 46px);
