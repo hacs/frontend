35d8f902840a050147683b561f1f49c667d1fe3c
ludeeus <joasoe@gmail.com>
2019-12-19 11:34:57 +0000

    Change wording

--- src/misc/onboarding_steps/3.ts
+++ src/misc/onboarding_steps/3.ts
@@ -8,7 +8,7 @@ export const step = html`
   <p>
     The first time you start Home Assistant with HACS enabled it needs to get the information about every repository it knows.</br> it is important that you let it finish this before you restart Home Assistant, if your restart before it's finished it needs to start over.
   </p>
-  <p>When it's done you will no longer see the progressbar</p>
+  <p>When it's done you will no longer see the progressbar.</p>
   <hacs-progressbar .active=${true}></hacs-progressbar>
-  <p>This prosess can take several minuttes or hours depending on your system and network, be paitient</p>
+  <p>This prosess can take several minuttes depending on your system and network.</p>
 `;
