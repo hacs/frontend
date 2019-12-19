12022aa924c5186314a44c33071e1cf6c304c561
Ryan Meek <25127328+maykar@users.noreply.github.com>
2019-12-19 09:53:38 -0500

    Update 0.ts

--- src/misc/onboarding_steps/0.ts
+++ src/misc/onboarding_steps/0.ts
@@ -6,10 +6,10 @@ export const step = html`
   </h1>
   <hr />
   <p>
-    Congratulations! you have sucessfully installed HACS 🎉 </br> This onboarding
-    prosess will hopefully help you understand what HACS is and what it does.
+    Congratulations! you have successfully installed HACS 🎉 </br> This onboarding
+    process will hopefully help you understand what HACS is and what it does.
   </p>
-  <p>To start off here are a few usefull links</p>
+  <p>To start off here are a few useful links</p>
   <li><hacs-link url="https://hacs.xyz/">HACS Documentation</hacs-link></li>
   <li><hacs-link url="https://github.com/hacs">HACS @ GitHub</hacs-link></li>
   <li><hacs-link url="https://discord.gg/apgchf8">HACS @ Discord</hacs-link></li>
