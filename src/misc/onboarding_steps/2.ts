6e96a9e11a6474a400de83ced0c09b9269e21899
ludeeus <joasoe@gmail.com>
2019-12-27 16:09:12 +0000

    Smal typo corrections in onboarding

--- src/misc/onboarding_steps/2.ts
+++ src/misc/onboarding_steps/2.ts
@@ -6,7 +6,7 @@ export const step = html`
   </h1>
   <hr />
   <p>
-    If you see this progress bar it means that HACS is working with something in
+    If you see this progressbar it means that HACS is working with something in
     the background. Upgrade and uninstall are disabled while this is running.
   </p>
   <hacs-progressbar .active=${true}></hacs-progressbar>
--- src/misc/onboarding_steps/3.ts
+++ src/misc/onboarding_steps/3.ts
@@ -8,8 +8,8 @@ export const step = html`
   <p>
     The first time you start Home Assistant with HACS enabled it needs to get information about every repository it knows.</br> It is important that you let it finish this process before you restart Home Assistant, if you restart before it's finished it will need to start over.
   </p>
-  <p>When it's done you will no longer see the progress bar.</p>
+  <p>When it's done you will no longer see the progressbar.</p>
   <hacs-progressbar .active=${true}></hacs-progressbar>
   <p>This process can take several minutes depending on your system and network.</p>
-  <p>Again this processbar is just an example, you can click "Continue" now.</p>
+  <p>Again this progressbar is just an example, you can click "Continue" now.</p>
 `;
