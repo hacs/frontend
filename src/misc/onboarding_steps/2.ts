05cfa3bf0ba3afeac9a8d588c26ad89536676405
Alex Camilleri <camilleri.alex@gmail.com>
2020-02-09 23:16:02 +0100

    Formatting
    Removing white spaces

--- src/misc/onboarding_steps/2.ts
+++ src/misc/onboarding_steps/2.ts
@@ -6,13 +6,13 @@ export const step = html`
   </h1>
   <hr />
   <p>
-	  On the first startup HACS needs to gather information about every extentions repository it knows.</br>
+    On the first startup HACS needs to gather information about every extentions repository it knows.</br>
     This process can take several minutes depending on your system and network. 
     Don't restart Home Assistant or the process will have to start over again.
   </p>
   <hacs-progressbar .active=${true}></hacs-progressbar>
   <p>
-	  Whenever you see this progressbar, it means that HACS is working in the background. 
+    Whenever you see this progressbar, it means that HACS is working in the background. 
     The progressbar will disappear once the process is finished (Upgrade and Uninstall options 
     will be disabled until then).</p> 
   </p>
