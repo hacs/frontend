6894f7e003a928cb663d69bde615dfbefd561983
Alex Camilleri <camilleri.alex@gmail.com>
2020-02-09 23:15:22 +0100

    First Startup merged with Progress bar
    Two steps merged into one

--- src/misc/onboarding_steps/2.ts
+++ src/misc/onboarding_steps/2.ts
@@ -2,13 +2,23 @@ import { html } from "lit-element";
 
 export const step = html`
   <h1>
-    Background tasks
+    First startup
   </h1>
   <hr />
   <p>
-    If you see this progressbar it means that HACS is working with something in
-    the background. Upgrade and uninstall are disabled while this is running.
+	  On the first startup HACS needs to gather information about every extentions repository it knows.</br>
+    This process can take several minutes depending on your system and network. 
+    Don't restart Home Assistant or the process will have to start over again.
   </p>
   <hacs-progressbar .active=${true}></hacs-progressbar>
-  <p>This is just an example, you can click "Continue" now.</p>
+  <p>
+	  Whenever you see this progressbar, it means that HACS is working in the background. 
+    The progressbar will disappear once the process is finished (Upgrade and Uninstall options 
+    will be disabled until then).</p> 
+  </p>
+  <p>The progressbar above is just an example, you can click "Continue" to move to the next step.</p>
+  <p>
+    NB!: While you read through this onboarding section, HACS is already getting information 
+    about the repositories it knows, so you will soon see items appear in the HACS page!
+  </p>
 `;
