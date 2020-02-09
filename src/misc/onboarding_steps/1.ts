4f83abc9fd25d7d05ce2ebc57f6ca2a69fec12d8
Alex Camilleri <camilleri.alex@gmail.com>
2020-02-09 23:08:05 +0100

    Reordered extentions list and added Warning step
    Moved Warning step here from step 5

--- src/misc/onboarding_steps/1.ts
+++ src/misc/onboarding_steps/1.ts
@@ -6,20 +6,30 @@ export const step = html`
   </h1>
   <hr />
   <p>
-    HACS, in short, is really just a glorified downloader, but no one wants
-    that so....
+    HACS is a distribution platform that makes it super easy 
+	  to find, install and upgrade custom extensions for Home Assistant.
   </p>
   <p>
-    HACS works as a distribution platform for extensions to Home Assistant that
-    are not part of Home Assistant core.
+    HACS is a distribution platform that makes it super easy to find, install and upgrade 
+	  custom extensions that are not part of Home Assistant core.
   </p>
   <p>
-    Extensions, or "categories", in this context are:
+    Extensions, in this context, are:
   </p>
-  <li>Custom integrations (custom_components)</li>
   <li>
     <hacs-link url="https://www.home-assistant.io/lovelace"
       >Plugins (Lovelace elements)</hacs-link
+    >
+  </li>
+  <li>
+    <hacs-link
+      url="https://www.home-assistant.io/integrations/frontend/#defining-themes"
+      >Themes</hacs-link
+    >
+  </li>
+  <li>
+    <hacs-link url="https://developers.home-assistant.io/docs/en/creating_integration_file_structure.html"
+    >Custom integrations (custom_components)</hacs-link
     >
   </li>
   <li>
@@ -29,22 +39,26 @@ export const step = html`
   </li>
   <li>
     <hacs-link url="https://www.home-assistant.io/integrations/python_script/"
-      >python scripts</hacs-link
+      >Python scripts</hacs-link
     >
   </li>
-  <li>
-    <hacs-link
-      url="https://www.home-assistant.io/integrations/frontend/#defining-themes"
-      >Themes</hacs-link
-    >
-  </li>
+  </br>
+  <h1>
+	  🚨 Warning
+  </h1>
+  <hr />
   <p>
-    Having HACS as a distribution platform allows the developer(s)/author(s) of
-    these extensions to reach a broader audience and allows for fast deployment
-    of new versions.
+	  HACS and everything you find in it are <b>not</b> tested by Home Assistant.
   </p>
   <p>
-  That's fine for developers/authors, but how about users?</br>
-  The nice part for users is that most of the custom extensions for Home Assistant now are in HACS, which makes it super easy to find, install, and upgrade.
+	  Custom extentions can cause harm to your system and/or contain bugs.
+	  If you have issues with a custom extension you found in HACS, 
+    report it to the author of <i>that</i> custom extension.
+  </p>
+  <p>
+	  You can easily get to the issue tracker of anything you find 
+    in HACS with the menu (located at the top right) on every repository you find inside HACS.
+	  </br></br>
+	  <b>The HACS and Home Assistant teams do not support <i>anything</i> you find here.</b>
   </p>
 `;
