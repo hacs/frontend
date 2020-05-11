b9eadd04aae868c06a1b6277a2bcd3fb9ee09788
ludeeus <joasoe@gmail.com>
2020-05-11 09:49:58 +0000

    Remove legacy lovelace actions

--- src/components/buttons/HacsButtonAddToLovelace.ts
+++ src/components/buttons/HacsButtonAddToLovelace.ts
@@ -2,8 +2,6 @@ import { customElement, TemplateResult, html, property } from "lit-element";
 import swal from "sweetalert";
 import { HacsRepositoryButton } from "./HacsRepositoryButton";
 
-import { LovelaceConfig, LovelaceResourceConfig } from "../../data";
-
 import { localize } from "../../localize/localize";
 
 import { Logger } from "../../misc/Logger";
@@ -16,54 +14,24 @@ export class HacsButtonAddToLovelace extends HacsRepositoryButton {
 
     return html`
       <mwc-button @click=${this.RepositoryAddToLovelace}>
-        Add to Lovelace
+        ${localize("repository.add_to_lovelace")}
       </mwc-button>
     `;
   }
 
   async RepositoryAddToLovelace() {
     const value = await swal(localize("confirm.add_to_lovelace"), {
-      buttons: [localize("confirm.no"), localize("confirm.yes")]
+      buttons: [localize("confirm.no"), localize("confirm.yes")],
     });
 
     if (value !== null) {
-      if (this.hass.config.version.split(".")[1] <= "106") {
-        this.hass.connection
-          .sendMessagePromise({
-            type: "lovelace/config",
-            force: false
-          })
-          .then(
-            resp => {
-              var currentConfig = resp as LovelaceConfig;
-              const cardConfig: LovelaceResourceConfig = {
-                type: "module",
-                url: `/hacsfiles/${this.repository.full_name.split("/")[1]}/${
-                  this.repository.file_name
-                }`
-              };
-              if (currentConfig.resources)
-                currentConfig.resources!.push(cardConfig);
-              else currentConfig.resources = [cardConfig];
-
-              this.hass.callWS({
-                type: "lovelace/config/save",
-                config: currentConfig
-              });
-            },
-            err => {
-              this.logger.error(err);
-            }
-          );
-      } else {
-        this.hass.callWS({
-          type: "lovelace/resources/create",
-          res_type: "module",
-          url: `/hacsfiles/${this.repository.full_name.split("/")[1]}/${
-            this.repository.file_name
-          }`
-        });
-      }
+      this.hass.callWS({
+        type: "lovelace/resources/create",
+        res_type: "module",
+        url: `/hacsfiles/${this.repository.full_name.split("/")[1]}/${
+          this.repository.file_name
+        }`,
+      });
       this.dispatchEvent(
         new CustomEvent("hacs-force-reload", { bubbles: true, composed: true })
       );
--- src/components/buttons/HacsButtonUninstall.ts
+++ src/components/buttons/HacsButtonUninstall.ts
@@ -9,7 +9,7 @@ import { HacsStyle } from "../../style/hacs-style";
 import { HacsRepositoryButton } from "./HacsRepositoryButton";
 import { RepositoryWebSocketAction } from "../../tools";
 import swal from "sweetalert";
-import { LovelaceConfig, LovelaceResourceConfig } from "../../data";
+import { LovelaceResourceConfig } from "../../data";
 import { localize } from "../../localize/localize";
 
 @customElement("hacs-button-uninstall")
@@ -83,52 +83,29 @@ export class HacsButtonUninstall extends HacsRepositoryButton {
     }
     RepositoryWebSocketAction(this.hass, this.repository.id, "uninstall");
   }
+
   async RepositoryRemoveFromLovelace() {
     const url1 = `/community_plugin/${
       this.repository.full_name.split("/")[1]
     }/${this.repository.file_name}`;
+
     const url2 = `/hacsfiles/${this.repository.full_name.split("/")[1]}/${
       this.repository.file_name
     }`;
-    if (this.hass.config.version.split(".")[1] <= "106") {
-      this.hass.connection
-        .sendMessagePromise({
-          type: "lovelace/config",
-          force: false,
-        })
-        .then((resp) => {
-          const currentConfig = resp as LovelaceConfig;
 
-          if (currentConfig.resources) {
-            const resources: LovelaceResourceConfig[] = currentConfig.resources.filter(
-              (element: LovelaceResourceConfig) => {
-                if (element.url === url1 || element.url === url2) {
-                  return false;
-                } else return true;
-              }
-            );
-            currentConfig.resources = resources;
-            this.hass.callWS({
-              type: "lovelace/config/save",
-              config: currentConfig,
-            });
-          }
-        });
-    } else {
-      const resources = await this.hass.callWS<LovelaceResourceConfig[]>({
-        type: "lovelace/resources",
+    const resources = await this.hass.callWS<LovelaceResourceConfig[]>({
+      type: "lovelace/resources",
+    });
+
+    const resource: LovelaceResourceConfig = resources.find(function (element) {
+      return element.url === url1 || element.url === url2;
+    });
+
+    if (resource) {
+      this.hass.callWS({
+        type: "lovelace/resources/delete",
+        resource_id: resource.id,
       });
-      const resource: LovelaceResourceConfig = resources.find(function (
-        element
-      ) {
-        return element.url === url1 || element.url === url2;
-      });
-      if (resource) {
-        this.hass.callWS({
-          type: "lovelace/resources/delete",
-          resource_id: resource.id,
-        });
-      }
     }
   }
 }
