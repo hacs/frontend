4e929e8c899dfe9e8e87d2380d8c4821aa496541
Joakim Sørensen <joasoe@gmail.com>
2020-05-24 12:36:04 +0200

    Update dialogs (#144)
    * update dialogs
    
    * Fix more dialog issues
    
    * Change background color
    
    * Fix defaults with sidebar missing from app

--- src/components/dialogs/hacs-dialog.ts
+++ src/components/dialogs/hacs-dialog.ts
@@ -4,6 +4,7 @@ import {
   customElement,
   html,
   TemplateResult,
+  property,
 } from "lit-element";
 import { classMap } from "lit-html/directives/class-map";
 import { HacsDialogBase } from "./hacs-dialog-base";
@@ -12,61 +13,72 @@ import { HacsCommonStyle } from "../../styles/hacs-common-style";
 
 @customElement("hacs-dialog")
 export class HacsDialog extends HacsDialogBase {
+  @property({ type: Boolean }) public noActions: boolean = false;
+  @property({ type: Boolean }) public hasContent: boolean = true;
+  @property({ type: Boolean }) public dynamicHeight: boolean = false;
+
   protected render(): TemplateResult | void {
     if (!this.active) {
       return html``;
     }
 
-    const sidebarDocked =
-      window.localStorage.getItem("dockedSidebar") === '"docked"';
+    const sidebar = window.localStorage.getItem("dockedSidebar");
+
+    const sidebarDocked = sidebar === '"docked"';
 
     return html`
       <div
         class=${classMap({
           backdrop: true,
           docked: sidebarDocked,
+          "full-width": sidebar === '"always_hidden"',
           narrow: this.narrow,
         })}
       >
         <ha-card
           class=${classMap({
             dialog: true,
+            "has-content": this.hasContent,
+            "dynamic-height": this.dynamicHeight,
             narrow: this.narrow,
           })}
         >
-          <div class="content">
-            <div class="header-group">
-              <ha-icon-button
-                class="close"
-                icon="mdi:close"
-                @click=${this._close}
-              ></ha-icon-button>
-              <div
-                class=${classMap({
-                  header: true,
-                  "narrow-header": this.narrow,
-                })}
-              >
-                <slot name="header"></slot>
-              </div>
-            </div>
+          <div class="header-group">
+            <ha-icon-button
+              class="close"
+              icon="mdi:close"
+              @click=${this._close}
+            ></ha-icon-button>
+
             <div
               class=${classMap({
-                "card-content": true,
-                "narrow-content": this.narrow,
+                header: true,
+                "narrow-header": this.narrow,
               })}
             >
-              <slot></slot>
-            </div>
-            <div
-              class=${classMap({
-                "card-actions": true,
-                "narrow-actions": this.narrow,
-              })}
-            >
-              <slot name="actions"></slot>
+              <slot name="header"></slot>
             </div>
           </div>
+          <div
+            class=${classMap({
+              "card-content": true,
+              noactions: this.noActions,
+              "dynamic-height": !this.narrow && this.dynamicHeight,
+              "narrow-content": this.narrow,
+            })}
+          >
+            <div class="content"><slot></slot></div>
+          </div>
+          ${!this.noActions
+            ? html` <div
+                class=${classMap({
+                  "card-actions": true,
+                  "narrow-actions": this.narrow,
+                })}
+              >
+                <slot name="actions"></slot>
+              </div>`
+            : ""}
         </ha-card>
       </div>
     `;
@@ -86,7 +98,11 @@ export class HacsDialog extends HacsDialogBase {
       HacsCommonStyle,
       css`
         ha-card {
-          background-color: var(--paper-card-background-color);
+          background-color: var(
+            --paper-dialog-background-color,
+            var(--card-background-color)
+          );
+          transition: none;
         }
         .header-group {
           position: absolute;
@@ -124,20 +140,36 @@ export class HacsDialog extends HacsDialogBase {
         }
 
         .card-content {
-          padding: 72px 12px 12px 12px;
-          max-width: 100%;
+          position: relative;
+          padding: 0;
+          width: 100%;
+          padding: 0;
+          height: calc(100% - 136px);
+          overflow-y: auto;
+          overflow-x: hidden;
+          -webkit-overflow-scrolling: touch;
+          margin-top: 65px;
+        }
+        ha-card.dynamic-height .noactions {
+          margin-bottom: -65px;
+        }
+        .noactions {
+          height: calc(100% - 68px);
+        }
+        .content {
+          padding: 8px;
+          height: fit-content;
         }
 
         ::slotted([slot="actions"]) {
           position: absolute;
-          width: 100%;
+          width: calc(100% - 32px);
           display: flex;
           justify-content: space-between;
           align-items: center;
           font-size: 20px;
           height: 65px;
           background-color: var(--sidebar-background-color);
-          border-top: 1px solid var(--divider-color);
           font-weight: 400;
           color: var(--sidebar-text-color);
           font-family: var(--paper-font-body1_-_font-family);
@@ -151,13 +183,8 @@ export class HacsDialog extends HacsDialogBase {
           position: fixed;
           z-index: 1;
           top: 0;
-          left: 64px;
-          width: calc(100% - 64px);
-        }
-
-        .docked {
-          left: 256px !important;
-          width: calc(100% - 256px) !important;
+          left: var(--app-drawer-width);
+          width: calc(100% - var(--app-drawer-width));
         }
 
         .narrow {
@@ -172,16 +199,32 @@ export class HacsDialog extends HacsDialogBase {
         }
 
         .dialog {
-          top: 64px;
-          min-width: 350px;
-          max-height: calc(90% - 64px);
-          max-width: 90%;
-          padding-bottom: 65px;
-          z-index: 2;
-          overflow-x: hidden;
-          height: auto;
-          width: fit-content;
+          height: 300px;
           margin: auto;
+          max-height: calc(100% - 130px);
+          max-width: 90%;
+          min-width: 350px;
+          overflow-x: hidden;
+          padding-bottom: 65px;
+          padding: 0;
+          padding: unset;
+          top: 64px;
+          width: fit-content;
+          z-index: 2;
+        }
+        .has-content {
+          height: var(--hacs-dialog-height, -webkit-fill-available);
+        }
+
+        .dynamic-height {
+          height: auto;
+        }
+        ha-card.dynamic-height {
+          padding-bottom: 65px;
+        }
+
+        .full-width {
+          --app-drawer-width: 0%;
         }
       `,
     ];
