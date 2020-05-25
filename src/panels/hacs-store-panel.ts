9d6ed321efaa9d83d93806ee731c5c7c32bc4343
Joakim Sørensen <joasoe@gmail.com>
2020-05-25 17:02:35 +0200

    Fix overflow issue with card-action padding (#165)

--- src/panels/hacs-store-panel.ts
+++ src/panels/hacs-store-panel.ts
@@ -161,7 +161,7 @@ export class HacsStorePanel extends LitElement {
         }
         .content {
           display: grid;
-          grid-template-columns: repeat(auto-fit, minmax(350px, 1fr));
+          grid-template-columns: repeat(auto-fit, minmax(360px, 1fr));
           grid-gap: 16px 16px;
           padding: 8px 16px 16px;
           margin-bottom: 64px;
