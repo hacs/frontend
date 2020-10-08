fbc79d36ac630958ad0121adbfc2f0591465514e
Kendell R <KTibow@users.noreply.github.com>
2020-10-08 10:51:41 -0700

    Use ha-card-border-width

--- src/components/hacs-repository-card.ts
+++ src/components/hacs-repository-card.ts
@@ -361,7 +361,7 @@ export class HacsRepositoryCard extends LitElement {
 
         .status-border {
           border-style: solid;
-          border-width: 1px;
+          border-width: var(--ha-card-border-width);
         }
 
         .status-header {
