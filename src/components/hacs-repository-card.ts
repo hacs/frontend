d28788776cff19d253c1ebe1bb905395af138fc6
Kendell R <KTibow@users.noreply.github.com>
2020-10-17 07:01:43 -0700

    Update hacs-repository-card.ts

--- src/components/hacs-repository-card.ts
+++ src/components/hacs-repository-card.ts
@@ -361,7 +361,7 @@ export class HacsRepositoryCard extends LitElement {
 
         .status-border {
           border-style: solid;
-          border-width: var(--ha-card-border-width, 1px);
+          border-width: min(var(--ha-card-border-width, 1px), 10px);
         }
 
         .status-header {
