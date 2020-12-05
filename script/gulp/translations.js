afda028b760a40151eed4418ace33d50ef76b9c9
Ludeeus <ludeeus@ludeeus.dev>
2020-12-05 16:23:53 +0000

    Really fix it..

--- script/gulp/translations.js
+++ script/gulp/translations.js
@@ -3,7 +3,7 @@ const fs = require("fs-extra");
 const del = require("del");
 const log = require("fancy-log")
 
-const changeLang = {"et-EE": "et"}
+const changeLang = {"et_EE": "et"}
 
 gulp.task("generate-translations", async function (task) {
   del.sync("./src/localize/generated.ts");
