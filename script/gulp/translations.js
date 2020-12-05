6b1b92497ebe443976b8bc541154e6dd80b40ba6
Ludeeus <ludeeus@ludeeus.dev>
2020-12-05 16:10:06 +0000

    Fix wrong ISO code for estonian

--- script/gulp/translations.js
+++ script/gulp/translations.js
@@ -1,14 +1,22 @@
 const gulp = require("gulp");
 const fs = require("fs-extra");
 const del = require("del");
+const log = require("fancy-log")
+
+const changeLang = {"et-EE": "et"}
 
 gulp.task("generate-translations", async function (task) {
   del.sync("./src/localize/generated.ts");
   const files = await fs.readdir("./src/localize/languages");
   const languages = {};
   files.forEach((file) => {
-    const lang = file.split(".")[0];
+    let lang = file.split(".")[0];
+    if (changeLang[lang]) {
+      log(`Language code '${lang}' is wrong, using '${changeLang[lang]}'`)
+      lang = changeLang[lang]
+    }
     languages[lang] = fs.readJSONSync(`./src/localize/languages/${file}`, "utf-8");
+    log(`Generating transtions for '${lang}'`)
   });
   await fs.writeFile(
     "./src/localize/generated.ts",
