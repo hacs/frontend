40b401562e55a5e545fc66600960fc96b6a3ba16
Ludeeus <ludeeus@ludeeus.dev>
2021-08-22 16:10:00 +0000

    Guard for missing configuration

--- src/panels/hacs-sections.ts
+++ src/panels/hacs-sections.ts
@@ -6,52 +6,50 @@ import { Route } from "../../homeassistant-frontend/src/types";
 import { Configuration } from "../data/common";
 import { localize } from "../localize/localize";
 
-export const sections = (language: string) => {
-  return {
-    updates: [],
-    messages: [],
-    subsections: {
-      main: [
-        {
-          categories: ["integration"],
-          iconPath: mdiPuzzle,
-          id: "integrations",
-          info: localize(language, "sections.integrations.description"),
-          name: localize(language, "sections.integrations.title"),
-          path: "/hacs/integrations",
-          core: true,
-        },
-        {
-          categories: ["plugin", "theme"],
-          iconPath: mdiPalette,
-          id: "frontend",
-          info: localize(language, "sections.frontend.description"),
-          name: localize(language, "sections.frontend.title"),
-          path: "/hacs/frontend",
-          core: true,
-        },
-        {
-          categories: ["python_script", "appdaemon", "netdaemon"],
-          iconPath: mdiRobot,
-          id: "automation",
-          info: localize(language, "sections.automation.description"),
-          name: localize(language, "sections.automation.title"),
-          path: "/hacs/automation",
-          core: true,
-        },
-      ],
-    },
-  };
-};
+export const sections = (language: string) => ({
+  updates: [],
+  messages: [],
+  subsections: {
+    main: [
+      {
+        categories: ["integration"],
+        iconPath: mdiPuzzle,
+        id: "integrations",
+        info: localize(language, "sections.integrations.description"),
+        name: localize(language, "sections.integrations.title"),
+        path: "/hacs/integrations",
+        core: true,
+      },
+      {
+        categories: ["plugin", "theme"],
+        iconPath: mdiPalette,
+        id: "frontend",
+        info: localize(language, "sections.frontend.description"),
+        name: localize(language, "sections.frontend.title"),
+        path: "/hacs/frontend",
+        core: true,
+      },
+      {
+        categories: ["python_script", "appdaemon", "netdaemon"],
+        iconPath: mdiRobot,
+        id: "automation",
+        info: localize(language, "sections.automation.description"),
+        name: localize(language, "sections.automation.title"),
+        path: "/hacs/automation",
+        core: true,
+      },
+    ],
+  },
+});
 
-export const sectionsEnabled = memoizeOne((language: string, config: Configuration) => {
-  return sections(language).subsections.main.filter((section) => {
+export const sectionsEnabled = memoizeOne((language: string, config: Configuration) =>
+  sections(language).subsections.main.filter((section) => {
     const categories = section.categories;
     return categories?.filter((category) => config?.categories?.includes(category)).length !== 0;
-  });
-});
+  })
+);
 
 export const activePanel = (language: string, route: Route) => {
   const section = route.path.replace("/", "");
-  return sections(language).subsections.main.find((panel) => panel.id == section);
+  return sections(language).subsections.main.find((panel) => panel.id === section);
 };
