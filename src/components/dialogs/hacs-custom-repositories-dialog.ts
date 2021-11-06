f7c7c9a25e663c77a867bd70b7909bda146ec413
Joakim Sørensen <joasoe@gmail.com>
2021-11-06 13:45:09 +0100

    Use ha-form for custom repository dialog (#511)

--- src/components/dialogs/hacs-custom-repositories-dialog.ts
+++ src/components/dialogs/hacs-custom-repositories-dialog.ts
@@ -1,16 +1,14 @@
 import "@material/mwc-button/mwc-button";
 import { mdiDelete, mdiGithub } from "@mdi/js";
-import "@polymer/paper-item/paper-icon-item";
-import "@polymer/paper-item/paper-item";
-import "@polymer/paper-item/paper-item-body";
-import "@polymer/paper-listbox/paper-listbox";
 import { css, html, PropertyValues, TemplateResult } from "lit";
-import { customElement, property, query } from "lit/decorators";
+import { customElement, property, state } from "lit/decorators";
 import { computeRTL } from "../../../homeassistant-frontend/src/common/util/compute_rtl";
 import "../../../homeassistant-frontend/src/components/ha-alert";
+import "../../../homeassistant-frontend/src/components/ha-form/ha-form";
+import type { HaFormSchema } from "../../../homeassistant-frontend/src/components/ha-form/types";
 import "../../../homeassistant-frontend/src/components/ha-paper-dropdown-menu";
+import "../../../homeassistant-frontend/src/components/ha-settings-row";
 import "../../../homeassistant-frontend/src/components/ha-svg-icon";
-import { brandsUrl } from "../../../homeassistant-frontend/src/util/brands-url";
 import { getRepositories, repositoryAdd, repositoryDelete } from "../../data/websocket";
 import { hacsIconStyle, scrollBarStyle } from "../../styles/element-styles";
 import "./hacs-dialog";
@@ -18,19 +16,16 @@ import { HacsDialogBase } from "./hacs-dialog-base";
 
 @customElement("hacs-custom-repositories-dialog")
 export class HacsCustomRepositoriesDialog extends HacsDialogBase {
-  @property() private _inputRepository?: string;
-
   @property() private _error: any;
 
-  @query("#add-input") private _addInput?: any;
-
-  @query("#category") private _addCategory?: any;
+  @state() private _addRepositoryData = { category: undefined, repository: undefined };
 
   shouldUpdate(changedProperties: PropertyValues) {
     return (
       changedProperties.has("narrow") ||
       changedProperties.has("active") ||
       changedProperties.has("_error") ||
+      changedProperties.has("_addRepositoryData") ||
       changedProperties.has("repositories")
     );
   }
@@ -38,12 +33,28 @@ export class HacsCustomRepositoriesDialog extends HacsDialogBase {
   protected render(): TemplateResult | void {
     if (!this.active) return html``;
     const repositories = this.repositories?.filter((repo) => repo.custom);
+    const addRepositorySchema: HaFormSchema[] = [
+      {
+        type: "string",
+        name: "repository",
+      },
+      {
+        type: "select",
+        name: "category",
+        optional: true,
+        options: this.hacs.configuration.categories.map((category) => [
+          category,
+          this.hacs.localize(`common.${category}`),
+        ]),
+      },
+    ];
     return html`
       <hacs-dialog
         .active=${this.active}
         .hass=${this.hass}
         .title=${this.hacs.localize("dialog_custom_repositories.title")}
-        hideActions
+        scrimClickAction
+        escapeKeyAction
       >
         <div class="content">
           <div class="list">
@@ -55,71 +66,42 @@ export class HacsCustomRepositoriesDialog extends HacsDialogBase {
             ${repositories
               ?.filter((repo) => this.hacs.configuration.categories.includes(repo.category))
               .map(
-                (repo) => html`<paper-icon-item>
-                  ${repo.category === "integration"
-                    ? html`
-                        <img
-                          loading="lazy"
-                          .src=${brandsUrl({
-                            domain: repo.domain,
-                            darkOptimized: this.hass.themes.darkMode,
-                            type: "icon",
-                          })}
-                          referrerpolicy="no-referrer"
-                          @error=${this._onImageError}
-                          @load=${this._onImageLoad}
-                        />
-                      `
-                    : html`<ha-svg-icon .path=${mdiGithub} slot="item-icon"></ha-svg-icon>`}
-                  <paper-item-body
-                    @click=${() => this._showReopsitoryInfo(String(repo.id))}
-                    three-line
-                    >${repo.name}
-                    <div secondary>${repo.description}</div>
-                    <div secondary>Category: ${repo.category}</div></paper-item-body
-                  >
+                (repo) => html`<ha-settings-row
+                  @click=${() => this._showReopsitoryInfo(String(repo.id))}
+                >
+                  ${!this.narrow
+                    ? html`<ha-svg-icon slot="prefix" .path=${mdiGithub}></ha-svg-icon>`
+                    : ""}
+                  <span slot="heading">${repo.name}</span>
+                  <span slot="description">${repo.full_name} (${repo.category})</span>
+
                   <mwc-icon-button @click=${() => this._removeRepository(repo.id)}>
                     <ha-svg-icon class="delete" .path=${mdiDelete}></ha-svg-icon>
                   </mwc-icon-button>
-                </paper-icon-item>`
+                </ha-settings-row>`
               )}
           </div>
-        </div>
-        <div class="add-repository" ?narrow=${this.narrow}>
-          <input
-            id="add-input"
-            class="add-input"
-            slot="secondaryaction"
-            placeholder="${this.hacs.localize("dialog_custom_repositories.url_placeholder")}"
-            .value=${this._inputRepository || ""}
-            @input=${this._inputValueChanged}
+          <ha-form
             ?narrow=${this.narrow}
-          />
-
-          <ha-paper-dropdown-menu
-            ?narrow=${this.narrow}
-            class="category"
-            label="${this.hacs.localize("dialog_custom_repositories.category")}"
+            .data=${this._addRepositoryData}
+            .schema=${addRepositorySchema}
+            .computeLabel=${(schema: HaFormSchema) =>
+              schema.name === "category"
+                ? this.hacs.localize("dialog_custom_repositories.category")
+                : this.hacs.localize("common.repository")}
+            @value-changed=${this._valueChanged}
           >
-            <paper-listbox id="category" slot="dropdown-content" selected="-1">
-              ${this.hacs.configuration.categories.map(
-                (category) => html`
-                  <paper-item class="categoryitem" .category=${category}>
-                    ${this.hacs.localize(`common.${category}`)}
-                  </paper-item>
-                `
-              )}
-            </paper-listbox>
-          </ha-paper-dropdown-menu>
-          <mwc-button
-            ?narrow=${this.narrow}
-            slot="primaryaction"
-            raised
-            @click=${this._addRepository}
-          >
-            ${this.hacs.localize("common.add")}
-          </mwc-button>
+          </ha-form>
         </div>
+        <mwc-button
+          slot="primaryaction"
+          raised
+          .disabled=${this._addRepositoryData.category === undefined ||
+          this._addRepositoryData.repository === undefined}
+          @click=${this._addRepository}
+        >
+          ${this.hacs.localize("common.add")}
+        </mwc-button>
       </hacs-dialog>
     `;
   }
@@ -128,27 +110,29 @@ export class HacsCustomRepositoriesDialog extends HacsDialogBase {
     this.hass.connection.subscribeEvents((msg) => (this._error = (msg as any).data), "hacs/error");
   }
 
-  private _inputValueChanged() {
-    this._inputRepository = this._addInput?.value;
+  private _valueChanged(ev) {
+    this._addRepositoryData = ev.detail.value;
   }
 
   private async _addRepository() {
     this._error = undefined;
-    const repository = this._inputRepository;
-    const category = this._addCategory?.selectedItem?.category;
-    if (!category) {
+    if (!this._addRepositoryData.category) {
       this._error = {
         message: this.hacs.localize("dialog_custom_repositories.no_category"),
       };
       return;
     }
-    if (!repository) {
+    if (!this._addRepositoryData.repository) {
       this._error = {
         message: this.hacs.localize("dialog_custom_repositories.no_repository"),
       };
       return;
     }
-    await repositoryAdd(this.hass, repository, category);
+    await repositoryAdd(
+      this.hass,
+      this._addRepositoryData.repository,
+      this._addRepositoryData.category
+    );
     this.repositories = await getRepositories(this.hass);
   }
 
@@ -156,16 +140,6 @@ export class HacsCustomRepositoriesDialog extends HacsDialogBase {
     this._error = undefined;
     await repositoryDelete(this.hass, repository);
     this.repositories = await getRepositories(this.hass);
-  }
-
-  private _onImageLoad(ev) {
-    ev.target.style.visibility = "initial";
-  }
-
-  private _onImageError(ev) {
-    if (ev.target) {
-      ev.target.outerHTML = `<ha-svg-icon path="${mdiGithub}" slot="item-icon"></ha-svg-icon>`;
-    }
   }
 
   private async _showReopsitoryInfo(repository: string) {
@@ -186,92 +160,33 @@ export class HacsCustomRepositoriesDialog extends HacsDialogBase {
       scrollBarStyle,
       hacsIconStyle,
       css`
-        .content {
-          width: 1024px;
-          display: contents;
-        }
         .list {
           position: relative;
-          margin-top: 16px;
+          margin-top: -16px;
           max-height: 870px;
           overflow: auto;
+        }
+        ha-form {
+          display: block;
+          padding-bottom: 25px;
+        }
+        ha-form[narrow] {
+          bottom: 0;
+          position: absolute;
+          width: calc(100% - 48px);
         }
         ha-svg-icon {
           --mdc-icon-size: 36px;
         }
-        img {
-          align-items: center;
-          display: block;
-          justify-content: center;
-          margin-bottom: 16px;
-          max-height: 36px;
-          max-width: 36px;
-          position: absolute;
+        ha-svg-icon:not(.delete) {
+          margin-right: 4px;
+        }
+        ha-settings-row {
+          cursor: pointer;
+          padding: 0;
         }
         .delete {
-          color: var(--hacs-error-color, var(--google-red-500));
-        }
-        paper-item-body {
-          cursor: pointer;
-        }
-
-        paper-item-body {
-          width: 100%;
-          min-height: var(--paper-item-body-two-line-min-height, 72px);
-          display: var(--layout-vertical_-_display);
-          flex-direction: var(--layout-vertical_-_flex-direction);
-          justify-content: var(--layout-center-justified_-_justify-content);
-        }
-        paper-item-body div {
-          font-size: 14px;
-          color: var(--secondary-text-color);
-        }
-        .add-repository {
-          display: grid;
-          width: 100%;
-          justify-items: right;
-        }
-
-        .add-input {
-          width: 100%;
-          height: 40px;
-          margin-top: 32px;
-          border: 0;
-          border-bottom: 1px var(--mdc-theme-primary) solid;
-          text-align: left;
-          padding: 0px;
-          font-size: initial;
-          color: var(--sidebar-text-color);
-          font-family: var(--paper-font-body1_-_font-family);
-        }
-        input:focus {
-          outline-offset: 0;
-          outline: 0;
-        }
-        input {
-          background-color: var(--sidebar-background-color);
-        }
-        ha-paper-dropdown-menu {
-          width: 100%;
-        }
-        mwc-button {
-          width: fit-content;
-          margin-top: 16px;
-        }
-
-        input[narrow],
-        .add-repository[narrow],
-        ha-paper-dropdown-menu[narrow],
-        mwc-button[narrow] {
-          margin: 0;
-          padding: 0;
-          left: 0;
-          top: 0;
-          width: 100%;
-          max-width: 100%;
-        }
-        .add-repository[narrow] {
-          display: contents;
+          color: var(--hcv-color-error);
         }
       `,
     ];
