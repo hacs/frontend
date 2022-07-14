dadfd5d45e661091c535cc5e5a81f112c9200abe
Joakim Sørensen <ludeeus@ludeeus.dev>
2022-07-14 10:00:25 +0200

    Update dialogs when _repository changes (#609)

--- src/components/dialogs/hacs-dialog-base.ts
+++ src/components/dialogs/hacs-dialog-base.ts
@@ -34,6 +34,7 @@ export class HacsDialogBase extends LitElement {
       changedProperties.has("params") ||
       changedProperties.has("_error") ||
       changedProperties.has("_progress") ||
+      changedProperties.has("_repository") ||
       changedProperties.has("_releaseNotes") ||
       changedProperties.has("_updating")
     );
