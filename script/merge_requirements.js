ca43e4a0301f7f5dc651c404bb5edfb04ba632ad
ludeeus <joasoe@gmail.com>
2024-01-11 21:30:07 +0000

    update

--- script/merge_requirements.js
+++ script/merge_requirements.js
@@ -9,22 +9,23 @@ const hacs = JSON.parse(rawhacs);
 fs.writeFileSync(
   ".yarnrc.yml",
   `
+compressionLevel: mixed
+
 defaultSemverRangePrefix: ""
+
+enableGlobalCache: false
 
 nodeLinker: node-modules
 
 yarnPath: ./homeassistant-frontend/.yarn/releases/yarn-${core.packageManager.split("@")[1]}.cjs
-`
+`,
 );
 
-fs.copyFileSync(
-  `./homeassistant-frontend/.browserslistrc`,
-  `.browserslistrc`
-);
+fs.copyFileSync(`./homeassistant-frontend/.browserslistrc`, `.browserslistrc`);
 
 fs.copyFileSync(
   `./homeassistant-frontend/src/translations/translationMetadata.json`,
-  `./src/localize/languages/translationMetadata.json`
+  `./src/localize/languages/translationMetadata.json`,
 );
 
 fs.writeFileSync(
@@ -32,14 +33,22 @@ fs.writeFileSync(
   JSON.stringify(
     {
       ...hacs,
-      resolutions: { ...Object.fromEntries(
-        Object.entries(core.resolutions).map(([key, val]) => [key, val.replace("#.yarn/patches/", "#./homeassistant-frontend/.yarn/patches/").replace("#./.yarn/patches/", "#./homeassistant-frontend/.yarn/patches/")]),
-      ), ...hacs.resolutionsOverride },
+      resolutions: {
+        ...Object.fromEntries(
+          Object.entries(core.resolutions).map(([key, val]) => [
+            key,
+            val
+              .replace("#.yarn/patches/", "#./homeassistant-frontend/.yarn/patches/")
+              .replace("#./.yarn/patches/", "#./homeassistant-frontend/.yarn/patches/"),
+          ]),
+        ),
+        ...hacs.resolutionsOverride,
+      },
       dependencies: { ...core.dependencies, ...hacs.dependenciesOverride },
       devDependencies: { ...core.devDependencies, ...hacs.devDependenciesOverride },
       packageManager: core.packageManager,
     },
     null,
-    2
-  )
+    2,
+  ),
 );
