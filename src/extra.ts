efba1ca116cbe483c1c31a11d977a5466d2a6b10
ludeeus <joasoe@gmail.com>
2024-01-12 06:53:47 +0000

    restructure

--- src/extra.ts
+++ src/extra.ts
@@ -2,30 +2,32 @@ import { mainWindow } from "../homeassistant-frontend/src/common/dom/get_main_wi
 import { showToast } from "../homeassistant-frontend/src/util/toast";
 
 (() => {
-  const haDocument = mainWindow?.document.querySelector("home-assistant");
+  const haDocument = mainWindow?.document?.querySelector("home-assistant");
   const hass = haDocument?.hass;
-  if (!hass) {
-    console.error("[HACS/reload_handler] hass not found");
-    return;
-  }
-  if ((haDocument as any).___hacs_reload_handler_active) {
-    return;
-  }
-  (haDocument as any).___hacs_reload_handler_active = true;
-  hass.connection.subscribeEvents((ev) => {
-    console.log(`[HACS/reload_handler] ${JSON.stringify(ev)}`);
-    showToast(haDocument, {
-      duration: 300 * 1000,
-      dismissable: false,
-      message: `HACS: ${
-        hass.localize("ui.panel.lovelace.reload_lovelace") || "You need to reload your browser"
-      }`,
-      action: {
-        action: () => {
-          mainWindow.location.href = mainWindow.location.href;
+
+  function reloadHandler() {
+    if ((haDocument as any).___hacs_reload_handler_active) {
+      return;
+    }
+    if (!hass) {
+      console.error("[HACS/extra/reload_handler] hass not found");
+      return;
+    }
+    (haDocument as any).___hacs_reload_handler_active = true;
+    hass.connection.subscribeEvents(() => {
+      showToast(haDocument, {
+        duration: 300 * 1000,
+        dismissable: false,
+        message: "[HACS] You need to reload your browser",
+        action: {
+          action: () => {
+            mainWindow.location.href = mainWindow.location.href;
+          },
+          text: "reload",
         },
-        text: "reload",
-      },
-    });
-  }, "hacs_resources_updated");
+      });
+    }, "hacs_resources_updated");
+  }
+
+  reloadHandler();
 })();
